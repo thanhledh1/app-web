4e7de697013a780f212e9b93fb87f58a3d4e4219
Thanh Le Van <thanh.levan@nfq.com>
2024-06-07 18:27:17 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MasterController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::post('/postlogin', [AuthController::class, 'postlogin'])->name('postlogin');
+Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
+Route::get('/create', [UserController::class, 'create'])->name('user.create');
+Route::post('/store', [UserController::class, 'store'])->name('user.store');
+
+Route::prefix('/')->middleware(['auth'])->group(function () {
+
+    // User
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/index', [UserController::class, 'index'])->name('user.index');
+        Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
+        Route::get('/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+        Route::put('/update/{id}', [UserController::class, 'update'])->name('user.update');
+        Route::delete('destroy/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+    });
+    // Menu
+    Route::group(['prefix' => 'menu'], function () {
+        Route::get('/', [MenuController::class, 'index'])->name('menu.index');
+        Route::get('/create', [MenuController::class, 'create'])->name('menu.create');
+        Route::post('/store', [MenuController::class, 'store'])->name('menu.store');
+        Route::get('/show/{id}', [MenuController::class, 'show'])->name('menu.show');
+        Route::get('/edit/{id}', [MenuController::class, 'edit'])->name('menu.edit');
+        Route::put('/edit/{id}', [MenuController::class, 'update'])->name('menu.admin.update');
+        Route::delete('destroy/{id}', [MenuController::class, 'destroy'])->name('menu.destroy');
+        Route::post('/update-order', [MenuController::class, 'updateOrder'])->name('menus.updateOrder');
+    });
 });
+
+// Master
+Route::group(['prefix' => 'master'], function () {
+    Route::get('/index', [MasterController::class, 'index'])->name('user.index');
+    Route::post('/{id}', [MasterController::class, 'update'])->name('menu.update');
+});
+
+
